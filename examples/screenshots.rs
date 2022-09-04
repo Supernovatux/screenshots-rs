8576aa771f6e22476f45cfcdb9e7d259823cb510
supernovatux <thulashitharan.d@gmail.com>
2022-09-04 11:54:38 +0530

    Changes

--- examples/screenshots.rs
+++ examples/screenshots.rs
@@ -9,11 +9,11 @@ fn main() {
     println!("capturer {:?}", screen);
     let mut image = screen.capture().unwrap();
     let mut buffer = image.buffer();
-    fs::write(format!("target/{}.png", screen.display_info.id), &buffer).unwrap();
+    //fs::write(format!("target/{}.png", screen.display_info.id), &buffer).unwrap();
 
     image = screen.capture_area(300, 300, 300, 300).unwrap();
     buffer = image.buffer();
-    fs::write(format!("target/{}-2.png", screen.display_info.id), &buffer).unwrap();
+    //fs::write(format!("target/{}-2.png", screen.display_info.id), &buffer).unwrap();
   }
 
   let screen = Screen::from_point(100, 100).unwrap();
@@ -21,7 +21,7 @@ fn main() {
 
   let image = screen.capture_area(300, 300, 300, 300).unwrap();
   let buffer = image.buffer();
-  fs::write("target/capture_display_with_point.png", &buffer).unwrap();
+  //fs::write("target/capture_display_with_point.png", &buffer).unwrap();
 
   println!("运行耗时: {:?}", start.elapsed());
 }
