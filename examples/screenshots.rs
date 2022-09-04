91c9b845d5649f527dae4a8ce0c0b9f465f7a2e6
supernovatux <thulashitharan.d@gmail.com>
2022-09-04 11:21:48 +0530

    Bench

--- examples/screenshots.rs
+++ examples/screenshots.rs
@@ -8,17 +8,17 @@ fn main() {
   for screen in screens {
     println!("capturer {:?}", screen);
     let mut image = screen.capture().unwrap();
-    image.save(format!("target/{}.png", screen.display_info.id)).unwrap();
+    //image.save(format!("target/{}.png", screen.display_info.id)).unwrap();
 
     image = screen.capture_area(300, 300, 300, 300).unwrap();
-    image.save(format!("target/{}-2.png", screen.display_info.id)).unwrap();
+    //image.save(format!("target/{}-2.png", screen.display_info.id)).unwrap();
   }
 
   let screen = Screen::from_point(100, 100).unwrap();
   println!("capturer {:?}", screen);
 
   let image = screen.capture_area(300, 300, 300, 300).unwrap();
-  image.save("target/capture_display_with_point.png").unwrap();
+  //image.save("target/capture_display_with_point.png").unwrap();
 
   println!("运行耗时: {:?}", start.elapsed());
 }
